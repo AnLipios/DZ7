62c5a4d839795165e2be91607705b8df7e9608a1
Anna <ogonek0918@yandex.ru>
2022-12-14 23:41:08 +0700

    task47

---
+++ dzshka/task47/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+void PrintArray(double[,] mass)
+{
+    for (int m=0; m<mass.GetLength(0); m++)
+    {
+        for (int n=0; n< mass.GetLength(1); n++)
+        {
+            Write($"{mass[m,n]:f2} ");
+            }
+            WriteLine();
+    }
+    
+}
+
+void FillArray(double[,]mass)
+{
+    for (int m=0; m<mass.GetLength(0); m++)
+    {
+        for (int n=0; n<mass.GetLength(1); n++)
+        {
+            mass[m,n]=new Random().NextDouble()*2+-3;
+        }
+    }
+  
+}
+
+
+double[,] mass=new double[4,3];
+FillArray(mass);
+PrintArray (mass);
+
